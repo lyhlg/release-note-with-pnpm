8470a702902fcd6b0b3e448c7c0939e8cc2f3698
Lee Yong Hyun <lyhlg0201@gmail.com>
2024-05-06 20:34:20 +0900

    fix: updated

--- release.config.js
+++ release.config.js
@@ -3,12 +3,13 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ["main"],
+  repositoryUrl: 'https://github.com/lyhlg/release-note-with-pnpm.git',
   extends: "semantic-release-monorepo",
+  branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-    "@semantic-release/github"
+    // "@semantic-release/npm",
+    // "@semantic-release/github"
   ]
 };
